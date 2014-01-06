c2a748dde86d9edd70e9abeb17fef8001c69d500
anath <anathabensour@gmail.com>
2014-01-06 17:39:34 +0000

    webpage

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,9 +1,34 @@
+
+
+function Pick(name) {
+  this.name = name;
+}
+
+Pick.prototype.pairs = {
+  scissors: { paper: 'cuts', lizard: 'decapitates'},
+  paper:    { rock: 'covers', spock: 'disproves'},
+  lizard:   { spock: 'poisons', paper: 'eats' },
+  rock:     { scissors: 'smashes', lizard: 'crushes' },
+  spock:    { rock: 'vaporizes', scissors: 'melts' }
+}
+
+Pick.prototype.beats = function(otherPick) {
+  var verb = this.pairs[this.name][otherPick.name];
+
+  if(verb) {
+    return [this.name, verb, otherPick.name].join(' ');
+  } else {
+    return false;
+  }
+}
+
 function Player(name) {
   this.name = name;
   // console.log("New player created")
 }
 
-Player.prototype.picks = function() {
+Player.prototype.picks = function(playerPick) {
+	this.pick = new Pick(playerPick);
   // if(foo === bar) {
 
   // }
@@ -14,6 +39,14 @@ function Game(player1, player2) {
   this.player2 = player2;
 }
 
-Game.prototype.winner = function() {
-  return this.player1;
+Game.prototype.winner = function(player1, player2) {
+  	var p1Wins = this.player1.pick.beats(this.player2.pick);
+  	var p2Wins = this.player2.pick.beats(this.player1.pick);
+
+
+	if(p1Wins) return p1Wins;
+	if(p2Wins) return p2Wins;
+
+	return 'Draw';
 }
+
